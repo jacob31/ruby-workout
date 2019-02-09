7efcc803120e568434a5c8341088c6c03e5a959f
jacob31 <bensilbernagel@gmail.com>
2019-02-08 18:11:22 -0600

    tutorialspoint practice pt1

--- keywords.rb
+++ keywords.rb
@@ -1,35 +1,27 @@
-# keywords.rb
+=begin
+filename: keywords.rb
+author:   Ben Silbernagel
+contact:  bensilbernagel@gmail.com
+purpose:  to create an interactive program that uses command line instructions to look up how a ruby keyword is used.
+=end
+keywords = { Encoding: ["__ENCODING__"],
+            'Current file' => ["__FILE__", "__LINE__"],
+            'Method syntax' => ["def", "end", "return", "self", "super", "yield"],
+            'Module syntax' => ["alias", "class", "end", "module", "undef"],
+            'Class syntax' => ["alias", "class", "end", "module", "undef"],
+            'Misc syntax' => ["BEGIN", "END", "defined?"],
+            'Boolean syntax' => ["and", "false", "not", "or", "true"],
+            'Exception handling syntax' => ["begin", "end", "ensure", "rescue", "retry"],
+            'Control expression syntax' => ["break", "case", "else", "elsif", "end", "for", "if", "in", "next", "redo", "then", \
+                                            "unless", "until", "when", "while"],
+            'Block syntax' => ["do", "end"],
+            'Literals' => ["false", "nil", "true"]
+          }
 
-puts "Encoding"
-puts "__ENCODING__"
+keywords.each do |k,v|
+  puts "#{k}: #{v}\n\n"
+end
 
-puts "Current file"
-puts "__FILE__, __LINE__"
-
-puts "methods syntax"
-puts "def, end, return, self, super, yield"
-
-puts "modules syntax"
-puts "alias, class, end, module, undef"
-
-puts "classes syntax"
-puts "alias, class, end, module, undef"
-
-puts "Misc syntax"
-puts "BEGIN, END, defined?,"
-
-puts "Boolean syntax"
-puts "and, false, not, or, true"
-
-puts "Exception handling syntax"
-puts "begin, end, ensure, rescue, retry"
-
-puts "Control expressions syntax"
-puts "break, case, else, elsif, end, for, if, in, next, redo, then, unless, until, when, while"
-
-puts "block syntax"
-puts "do, end"
-
-puts "literals"
-puts "false, nil, true"
-
+BEGIN {
+  puts "Ruby's Keywords\n\n"
+}
