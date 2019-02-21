e1346e001f35f9da95d5a801f0b199e26a35f4e4
jacob31 <bensilbernagel@gmail.com>
2019-02-20 19:55:43 -0600

    user input practice part 2

--- opposites.rb
+++ opposites.rb
@@ -1,37 +1,41 @@
 # opposites.rb
 
-def get_input
-  puts "Please enter a positive or negative integer:"
-  input = gets.chomp
-  return input
+def integer_request
+  loop do
+    puts "Please enter a positive or negative integer:"
+    integer = gets.chomp
+
+    return integer.to_i if valid_number?(integer)
+    puts "Invalid input. Only non-zero integers are allowed."
+  end
 end
 
 def valid_number?(number_string)
   number_string.to_i.to_s == number_string && number_string.to_i != 0
 end
 
-def non_zero_error
-  puts "Invalid input. Only non-zero integers are allowed."
-end
-
-loop do
-  first_int = get_input.to_i
-
-  second_int = get_input.to_i
-
-  if first_int == 0
-    non_zero_error()
-    first_int = get_input.to_i
-  elsif second_int == 0
-    non_zero_error()
-    second_int = get_input.to_i
-  else
-    puts"#{first_int} + #{second_int} = #{first_int + second_int}"
-    break
+def both_not_positive?(first_int, second_int)
+  if ( first_int.abs == first_int ) 
+    return true if ( -(second_int.abs) == second_int )
+  elsif ( -(first_int.abs) == first_int ) 
+    return true if ( second_int.abs == second_int )
   end
 end
 
+first_integer = nil
+second_integer = nil
 
-# varify input was not a 0 and make sure there is 1 negative and 1 positive integer.
+loop do
 
-# output to the screen the 2 integers and the sum of the 2 integers.
+  first_integer = integer_request()
+  second_integer = integer_request()
+  
+  break if both_not_positive?(first_integer, second_integer)
+  puts ">> Sorry. One integer must be positive, one must be negative."
+  puts ">> Please start over."
+
+end
+
+sum = first_integer + second_integer
+
+puts "#{first_integer} + #{second_integer} = #{sum}"
