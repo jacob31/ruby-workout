c669aa434f9baf48a077d4fdf05da5c041d7dbe4
jacob31 <bensilbernagel@gmail.com>
2019-03-05 20:17:32 -0600

    personal practice

---
+++ practice_1.rb
@@ -0,0 +1,18 @@
+# practice_1.rb
+
+=begin
+working on muscle memory. of strings in ruby
+=end
+
+puts "'puts' outputs text to the consle.\n\n"
+
+puts "To get user input use 'gets' & use the string method 'chomp' \
+      \nto remove the key stroke that is recorded by pressing 'Enter'.\n\n"
+
+puts "Let's see it in action. Please type your first name."
+first_name = gets.chomp.capitalize
+
+puts "\n\nWelcome #{first_name}."
+
+puts "\n\nThis is the code used to request and respond to our request for your name: \n\n"
+
